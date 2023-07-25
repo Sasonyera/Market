6d86c912b12abafd9c09c005c16fc8274cc92ff6
Alexis Garcia <garciaalexis942@gmail.com>
2023-07-25 16:38:57 -0500

    Primeros Archivos para empezar el proyecto con Java Spring

---
+++ src/main/java/com/platzi/market/PlatzMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzMarketApplication.class, args);
+	}
+
+}

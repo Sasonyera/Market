5fc10958e583af32cc4777dc5ef3d7149ad51324
Alexis Garcia <garciaalexis942@gmail.com>
2023-07-31 18:14:06 -0500

    Se implementaron Querys Methods, para obtener los productos por categoria de forma ascendente

--- src/main/java/com/platzi/market/PlatzMarketApplication.java
+++ src/main/java/com/platzi/market/PlatzMarketApplication.java
@@ -1,4 +1,4 @@
-package com.platzi.market.persistence;
+package com.platzi.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

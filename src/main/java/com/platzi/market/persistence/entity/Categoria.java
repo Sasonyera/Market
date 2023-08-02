efad0962a1b4118a75322df4dcf338870a3b6921
Alexis Garcia <garciaalexis942@gmail.com>
2023-08-02 17:58:42 -0500

    Se mapearon la API hacia el dominio, sigue el ERROR 2644.

--- src/main/java/com/platzi/market/persistence/entity/Categoria.java
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -18,6 +18,14 @@ public class Categoria {
     @OneToMany(mappedBy = "categoria")//relacion de categoria y productos. OneToMany - ManyToOne
     private List<Producto> productos;
 
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
     public Integer getIdCategoria() {
         return idCategoria;
     }
